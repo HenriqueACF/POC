9bf99c3c66c9d95815bbab30428d97e12565c06b
HenriqueACF <henrique.assis145@gmail.com>
2021-07-29 07:50:51 -0300

    init quasar

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,10 @@
 <template>
-  <div class="app">
-    <TopBar/>
-    <Section/>
-    <AreaButtons/>
-    <Warning />
-  </div>
+  <router-view />
 </template>
-
 <script>
-import TopBar from './components/TopBar.vue'
-import Section from './components/Section.vue'
-import AreaButtons from './components/AreaButtons.vue'
-import Warning from './components/Warning.vue'
+import { defineComponent } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    TopBar,
-    Section,
-    AreaButtons,
-    Warning
-  }
-}
+export default defineComponent({
+  name: 'App'
+})
 </script>
-
-<style>
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  .app{
-    background-color: #CCC;
-    height: 100vh;
-  }
-</style>
